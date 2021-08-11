f5affc65f4806b8b5941064bc1e5acbd2a516182
Airumian <a5s5r5@mail.ru>
2021-08-11 13:55:48 +0300

    Initial Commit

---
+++ ToDoLIst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoLIst
+//
+//  Created by Alexander Airumyan on 11.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
